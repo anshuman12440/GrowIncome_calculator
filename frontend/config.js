81f15a9dd3e63a300ede14402cd347a9ab6cc00e
Anshuman <anshumankumar.899@gmail.com>
2026-05-11 10:42:15 +0530

    Updates the config

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // Change this to http://localhost:3000 for local backend testing.
-window.API_BASE_URL = "https://growincome-calculator.onrender.com";
+window.API_BASE_URL = "https://growincome-calculator-1.onrender.com";
